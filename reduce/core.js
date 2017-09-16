be8202cdd792ca0ac1efd23310f7ead2285d4ae0
Zach Passarelli <zachary.passarelli@gmail.com>
2017-09-15 20:55:40 -0600

    complete reduce

--- reduce/core.js
+++ reduce/core.js
@@ -1,23 +1,32 @@
 function sum (array) {
-  return array.reduce(function(pre,cur){
-    return pre + cur;
+  return array.reduce(function(prev,curr){
+    return prev + curr;
   });
 }
 
 function productAll (array) {
-  return array.reduce(function(pre,cur){
-    return cur.reduce(function(pre2,cur2){
+  return array.reduce(function(prev,curr){
+    return prev * curr.reduce(function(pre2,cur2){
       return pre2 * cur2;
-    });
-  });
+    },1);
+  },1);
 }
 
 function objectify (array) {
-  return array;
+  return array.reduce(function(prev,curr){
+    prev[curr[0]] = curr[1];
+    return prev;
+  },{})
 }
 
 function luckyNumbers (array) {
-  return array;
+  return array.reduce(function(prev,curr,ind){
+    if(array.length - 1 === ind){
+      return prev + 'and ' + curr
+    } else {
+      return prev + curr + ', ';
+    }
+  },'Your lucky numbers are: ');
 
 }
 
