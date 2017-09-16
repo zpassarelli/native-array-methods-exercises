5f31d8ec5b675744bbd29d4b2ea23cd776718d99
Zach Passarelli <zachary.passarelli@gmail.com>
2017-09-15 21:09:25 -0600

    complete some

--- some/core.js
+++ some/core.js
@@ -2,28 +2,38 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+  return input.some(function(val){
+    return val > 10;
+  });
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  return input.some(function(val){
+    return val.length > 10;
+  });
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  return input.some(function(val){
+    return val.some(function(innerVal){
+      return innerVal == true;
+    })
+  });
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  return input.some(function(val){
+    return val.toLowerCase().includes('lost');
+  });
 };
 
 module.exports = {
